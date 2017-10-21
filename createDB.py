af6c9ec0cdd6aadc118ef929da5c4fe07a0c0aef
heysl <c.hasenfuss@posteo.de>
2017-10-21 18:00:44 +0200

    createDB ready

--- createDB.py
+++ createDB.py
@@ -2,14 +2,6 @@ import sqlite3
 import pymysql
 import configparser
 
-path = "settings.ini"
-
-config = configparser.ConfigParser()
-config.read(path)
-
-db_schema = config.get("db-schema", "schema")
-sqlite_db = config.get("sqlite", "file")
-
 def sqlite_create_db(db, schema):
     con = sqlite3.connect(db)
     with con:
@@ -24,5 +16,21 @@ def mysql_create_db(host, user, password, db, schema):
         with open(schema, 'r') as f:
             c.execute(f.read())
 
-sqlite_create_db(sqlite_db, db_schema)
+if __name__ == "__main__":
+    path = "settings.ini"
+
+    config = configparser.ConfigParser()
+    config.read(path)
+
+    db_schema = config.get("db-schema", "schema")
+    
+    sqlite_db = config.get("sqlite", "file")
+
+    mysql_host = config.get("mysql", "host")
+    mysql_user = config.get("mysql", "user")
+    mysql_password = config.get("mysql", "password")
+    mysql_db = config.get("mysql", "db")
+
+    sqlite_create_db(sqlite_db, db_schema)
+    mysql_create_db(mysql_host, mysql_user, mysql_password, mysql_db, db_schema)
 
